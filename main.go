f1ff265f2758abf14983a1058ad8de3f45684bec
Jake Mackin <canesfanjake1@gmail.com>
2026-04-21 13:22:54 -0400

    moved repl logic out of main.go into repl.go

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := cleanInput(scanner.Text())
-		if len(input) == 0 {
-			continue
-		}
-		fmt.Printf("Your command was: %v\n", input[0])
-	}
+	startRepl()
 }
